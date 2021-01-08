59f834478fd5ae44abd4d0b110b48bde89363f24
Kobayashi Mio <kobayashi@klar.co.jp>
2021-01-08 10:39:55 +0900

    記事投稿機能を実装

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use function PHPSTORM_META\map;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:50',
+            'body' => 'required|max:500',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'タイトル',
+            'body' => '本文',
+        ];
+    }
+}
